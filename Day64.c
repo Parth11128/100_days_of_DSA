8da995346474d35e262d6d6276ab254dbbd0a6b0
Parth11128 <parthsaxena11128@gmail.com>
2026-04-17 22:23:02 +0530

    Create Day64.c

---
+++ Day64.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(struct Node* adj[], int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+int queue[1000], front = 0, rear = 0;
+
+void enqueue(int x) {
+    queue[rear++] = x;
+}
+
+int dequeue() {
+    return queue[front++];
+}
+
+int isEmpty() {
+    return front == rear;
+}
+
+void bfs(int s, struct Node* adj[], int visited[]) {
+    enqueue(s);
+    visited[s] = 1;
+
+    while (!isEmpty()) {
+        int v = dequeue();
+        printf("%d ", v);
+
+        struct Node* temp = adj[v];
+        while (temp) {
+            if (!visited[temp->vertex]) {
+                visited[temp->vertex] = 1;
+                enqueue(temp->vertex);
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    struct Node* adj[n];
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v);
+    }
+
+    int visited[n];
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    int s;
+    scanf("%d", &s);
+
+    bfs(s, adj, visited);
+
+    return 0;
+}
